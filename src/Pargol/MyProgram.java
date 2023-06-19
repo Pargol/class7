b02bec7668edf4800c615c2af57dc10fc2cf22fe
pargol <pargol.poshtareh@gmail.com>
2023-06-19 11:50:16 -0400

    my class 7th

---
+++ src/Pargol/MyProgram.java
@@ -0,0 +1,27 @@
+package Pargol;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("what ever i am writing here");
+		System.out.println("pargol is the teacher " + "  and this is text i am aadding ");
+		System.out.print("Pargol is teaching java     ");
+		/*
+		this is my commnet ,msnas
+		i have many commnet
+		i have many commnet
+		i have many commnet
+		i have many commnet
+		i have many commnet
+		i have many commnet  
+		*/
+		/*
+		 6 + x = y
+		 */
+		
+		System.out.print("");
+		
+	}
+
+}
---
+++ src/Pargol/MyProgram2.java
@@ -0,0 +1,43 @@
+package Pargol;
+
+public class MyProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// x + 4 = y
+		String teacher = "Pargol Poshtareh";
+		System.out.println("My teacher name is " + teacher);
+
+		String course = "Programming 1";
+		System.out.print("I am studing ");
+		System.out.println(course);
+
+		// -2144783648 to 2144783648
+		int age = 18;
+
+		System.out.println("I am " + age + " years old");
+
+		int num_of_course = 10;
+		num_of_course = 33;
+		
+		System.out.println(num_of_course);
+		
+		double avg = 23.687874;  //
+		
+		System.out.println("the average is " + avg);
+		
+		float mygpa = 66f;
+		long numChar = 55553;
+		
+		String firstName = "Pargol";
+		String lastName = "Poshtareh";
+		
+		String fullName = firstName + " " + lastName;
+		System.out.print(fullName);
+		
+		
+		System.out.print(mygpa);
+		
+	}
+
+}
